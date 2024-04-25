451189ee6b348b86c272f64089cd708176da628e
ming <minghsu0107@gmail.com>
2024-04-25 10:34:54 -0700

    add reagent impl

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-from openai import OpenAI
+import httpx
 from pydantic import BaseModel
 
 app = FastAPI()
-
-client = OpenAI(api_key="ghu_WV2pVtYEMj0xPuG8ftVypa7UgPsyiK15rxTV",
-                base_url="http://localhost:8080/v1")
 
 
 class NutritionRecRequest(BaseModel):
@@ -14,15 +11,8 @@ class NutritionRecRequest(BaseModel):
 
 @app.post("/api/nutrition/recommendation")
 async def create_nutrition_recommendation(req: NutritionRecRequest):
-    resp = client.chat.completions.create(
-        model="gpt-4",
-        stream=False,
-        top_p=1.0,
-        max_tokens=100,
-        temperature=0.5,
-        messages=[
-            {"role": "system", "content": "You are a cat nutritionist, skilled in recommending healthy and balanced diets for pets."},
-            {"role": "user", "content": req.prompt}
-        ]
-    )
-    return resp.choices[0].message.content.strip()
+    async with httpx.AsyncClient() as client:
+        params = {
+            "key": "rg_v1_xglbbxx7o082u7qgn1ug10yl10putbntwmuq_ngk", "prompt": req.prompt}
+        resp = await client.get("https://noggin.rea.gent/shallow-chipmunk-1074", params=params)
+    return resp.text
