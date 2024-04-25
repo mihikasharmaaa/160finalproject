8b808f0f3914a525b8873d6faa887a3efdece638
刘钊禹 <liuzhaoyu@liuzhaoyudeMacBook-Air.local>
2024-04-25 12:43:00 -0700

    add

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import httpx
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 class NutritionRecRequest(BaseModel):
     prompt: str
-
 
 @app.post("/api/nutrition/recommendation")
 async def create_nutrition_recommendation(req: NutritionRecRequest):
